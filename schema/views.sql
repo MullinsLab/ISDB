8d72f860cf37836a87e363e19feee2a8a920a008
Thomas Sibley <trsibley@uw.edu>
2016-01-15 16:26:06 -0800

    Remove the total_in_gene and total_at_location columns from integration_summary
    total_in_gene was incorrectly double-counting (because it's applied
    after the GROUP BY) and total_at_location is just misleading and not
    super-useful.
    
    For both of these concepts, I think it makes more sense to use
    subqueries and normal aggregates.  For example, unique_sites in
    summary_by_gene is a better substitute for total_in_gene, and easier to
    understand how it's calculated.  total_at_location is more clearly
    disclosed by aggregating over integration_summary and counting when
    location is repeated.
    
    We can develop these and other potential views further as we develop
    more specs for useful reports.

--- schema/views.sql
+++ schema/views.sql
@@ -7,22 +7,14 @@ CREATE VIEW integration_summary AS
     SELECT source_name                                      AS source_name,
            sample->>'subject'                               AS subject,
            ncbi_gene.name                                   AS gene,
-           COUNT(1) OVER gene                               AS total_in_gene,
-
            COALESCE('chr' || chromosome, refseq_accession)  AS landmark,
            location                                         AS location,
-           COUNT(1) OVER location                           AS total_at_location,
-
            orientation_in_reference                         AS orientation_in_reference,
            orientation_in_gene                              AS orientation_in_gene,
            COUNT(1)                                         AS multiplicity
       FROM integration
       LEFT JOIN ncbi_gene USING (ncbi_gene_id)
      GROUP BY source_name, subject, gene, landmark, location, orientation_in_reference, orientation_in_gene
-    WINDOW gene     AS (PARTITION BY source_name, sample->>'subject', ncbi_gene.name),
-           location AS (PARTITION BY source_name, sample->>'subject', ncbi_gene.name,
-                                     COALESCE('chr' || chromosome, refseq_accession),
-                                     location)
 ;
 
 CREATE VIEW summary_by_gene AS
