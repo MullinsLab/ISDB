542d0e7d306b80d2d02536ecc1ccdeeb53724405
Thomas Sibley <trsibley@uw.edu>
2016-01-27 15:15:17 -0800

    Add a quick example of using the ISDB from R

---
+++ examples/summary-by-gene.R
@@ -0,0 +1,16 @@
+library(DBI)
+library(RPostgreSQL)
+library(dplyr)
+library(ggplot2)
+
+isdb = src_postgres("isdb", host = "ireland", user = "isdb_r")
+data = tbl(isdb, sql("SELECT * FROM summary_by_gene")) %>%
+  collect()
+
+genes = ggplot(data) +
+  geom_point(aes(x = unique_sites, y = subjects, size = total_in_gene)) +
+  scale_x_log10()
+
+plot(genes)
+
+dbDisconnect(isdb$con)
