3f9be599a7abddc59307b51fc6684abd3ffd9f52
Thomas Sibley <trsibley@uw.edu>
2016-02-12 15:38:00 -0800

    Prefix chromosomes with "chr" in landmark values
    This seems more in line with the practices of other people/tools and is
    somewhat more self-explanatory too (i.e. out of context, 4 is
    meaningless while chr4 is more meaningful).

--- schema/tables.sql
+++ schema/tables.sql
@@ -6,9 +6,9 @@ CREATE DOMAIN ltr_end AS text CHECK (
 
 CREATE DOMAIN landmark AS text CHECK (
     VALUE IN (
-         '1',  '2',  '3',  '4',  '5',  '6',  '7',  '8',  '9', '10',
-        '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
-        '21', '22',  'X',  'Y', 'MT'
+         'chr1',  'chr2',  'chr3',  'chr4',  'chr5',  'chr6',  'chr7',  'chr8',  'chr9', 'chr10',
+        'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20',
+        'chr21', 'chr22',  'chrX',  'chrY', 'chrMT'
     )
     OR
     -- Looks like a RefSeq accession for a complete region¹, but not one of the
