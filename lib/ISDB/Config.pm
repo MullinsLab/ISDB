89202a92c09d6ab9a83a815d4cc4f891503107f5
Thomas Sibley <trsibley@uw.edu>
2016-02-19 12:03:39 -0800

    Refactor config handling into ISDB::Config
    And standardize on config and config_local stems.  I'm about to need a
    couple more config items, so it seemed reasonable to centralize config
    handling.

---
+++ lib/ISDB/Config.pm
@@ -0,0 +1,46 @@
+use 5.010;
+package ISDB::Config;
+use Moo;
+use Types::Standard qw< :types >;
+use Types::Common::String qw< NonEmptyStr >;
+use Config::Any;
+use Hash::Merge qw< merge >;
+use List::Util qw< reduce pairvalues >;
+use Path::Tiny;
+use namespace::clean;
+
+has stems => (
+    is       => 'ro',
+    isa      => ArrayRef[NonEmptyStr],
+    required => 1,
+    default  => sub {[
+        # Stem order is important for precedence
+        map { path(__FILE__)->parent(3)->child($_)->stringify }
+            qw[ config_local config ]
+    ]},
+);
+
+has conf => (
+    is       => 'lazy',
+    isa      => HashRef,
+    init_arg => undef,
+);
+
+sub _build_conf {
+    my $self = shift;
+    my $conf = Config::Any->load_stems({
+        stems   => $self->stems,
+        use_ext => 1,
+
+        # Allow the use of [] to make Config::General settings explicitly an
+        # array even if only used once.
+        driver_args => {
+            General => { -ForceArray => 1 },
+        },
+    });
+    return reduce { merge($a, $b) } +{},
+              map { pairvalues %$_ }
+                  @$conf;
+}
+
+1;
