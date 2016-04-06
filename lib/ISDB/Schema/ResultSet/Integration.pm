c857e2038637efa33a9e8c609200a3476c485230
Thomas Sibley <trsibley@uw.edu>
2016-04-05 19:44:28 -0700

    Better display of sources to highlight publications
    Puts publications first and foremost instead of our internal concept of
    sources.  This is based on Jim's feedback in Slack on 29 March.

--- lib/ISDB/Schema/ResultSet/Integration.pm
+++ lib/ISDB/Schema/ResultSet/Integration.pm
@@ -7,25 +7,14 @@ package ISDB::Schema::ResultSet::Integration;
 use base 'DBIx::Class::ResultSet';
 use namespace::clean;
 
-sub counts_by_source {
-    my $self = shift;
-    my $me   = $self->current_source_alias;
-    return $self->search_rs({}, {
-        columns  => [ "$me.source_name", { count => { COUNT => 1 } } ],
-        group_by => "$me.source_name",
-        order_by => "$me.source_name",
-        prefetch => 'source',
-    });
-}
-
-sub counts_by_publication {
+sub counts_by_source_and_publication {
     my $self   = shift;
     my $me     = $self->current_source_alias;
-    return $self->publications->search_rs({}, {
-        '+columns' => [ { count => { COUNT => 1 } } ],
-        group_by => \["$me.sample->>'pubmed_id'"],
-        order_by => \["$me.sample->>'pubmed_id'"],
-        distinct => 0,
+    my $pubmed = "$me.sample->>'pubmed_id'";
+    return $self->search_rs({}, {
+        columns  => [ "$me.source_name", { pubmed_id => \[$pubmed] }, { count => { COUNT => 1 } } ],
+        group_by => [ "$me.source_name", $pubmed ],
+        order_by => [ "$pubmed != ''", "$me.source_name", $pubmed ],
     });
 }
 
