494ba6d76dd9dbb96f277cd5d07513e4fb441483
Thomas Sibley <trsibley@uw.edu>
2016-10-31 09:56:10 -0700

    ISDB::Schema: Explicitly enable UTF-8 support in DBD::Pg
    Everything expects it, and it's reasonable to make it explicit rather
    than implicit on the DBD::Pg version.
    
    Splits the default connection params out from the ->connect_default()
    method for re-usability.

--- lib/ISDB/Schema.pm
+++ lib/ISDB/Schema.pm
@@ -15,10 +15,18 @@ __PACKAGE__->load_namespaces;
 # Created by DBIx::Class::Schema::Loader v0.07043 @ 2015-12-02 17:00:10
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:v0gwY6SVU5Sc1FGqiUXwUw
 
-sub connect_default {
-    my $self   = shift;
+sub default_connection_info {
     my $dbname = $ENV{PGDATABASE} || 'isdb';
-    return $self->connect({ dsn => "dbi:Pg:dbname=$dbname" });
+    return {
+        dsn             => "dbi:Pg:dbname=$dbname",
+        pg_enable_utf8  => 1,
+        on_connect_do   => [],
+    };
+}
+
+sub connect_default {
+    my $self = shift;
+    return $self->connect( $self->default_connection_info );
 }
 
 1;
